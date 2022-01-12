4a196ec49a2244c76ef4cb5a60195d48274e3678
Rachael Moten <motenrachael@gmail.com>
2022-01-12 16:56:30 -0500

    Created javascript to show the current temperature

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = `<em>${response.data.name}</em>`;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+}
+let apiKey = `eb371e3285b37d59db8fd3917da1ca94`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
