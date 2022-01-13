ea4a3002a08d97453e7bb5929fcca68184a39967
Rachael Moten <motenrachael@gmail.com>
2022-01-13 15:58:53 -0500

    Changed date and time function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  return `${day} | ${hours} : ${minutes}`;
+}
 function search(city) {
   let apiKey = `eb371e3285b37d59db8fd3917da1ca94`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+  console.log(apiUrl);
 }
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 function displayTemperature(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -29,23 +45,19 @@ function displayTemperature(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", `${response.data.weather[0].main}`);
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let date = new Date();
-let day = days[date.getDay()];
-let hours = date.getHours();
-let minutes = date.getMinutes();
-document.querySelector("#date").innerHTML = `${day}`;
-document.querySelector("#time").innerHTML = `${hours}:${minutes}`;
-
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = Math.round((26 * 9) / 5 + 32);
+  document.querySelector("#temperature").innerHTML = fahrenheitTemperature;
+}
+search(`Orlando`);
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
