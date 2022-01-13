a43631e351054dfe78af2b35561cff23ae85675a
Rachael Moten <motenrachael@gmail.com>
2022-01-12 20:58:37 -0500

    Added search engine javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+function search(city) {
+  let apiKey = `eb371e3285b37d59db8fd3917da1ca94`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
 function displayTemperature(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -19,11 +30,6 @@ function displayTemperature(response) {
     .querySelector("#icon")
     .setAttribute("alt", `${response.data.weather[0].main}`);
 }
-let apiKey = `eb371e3285b37d59db8fd3917da1ca94`;
-let city = "Moscow";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
 
 let days = [
   "Sunday",
@@ -40,3 +46,6 @@ let hours = date.getHours();
 let minutes = date.getMinutes();
 document.querySelector("#date").innerHTML = `${day}`;
 document.querySelector("#time").innerHTML = `${hours}:${minutes}`;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
