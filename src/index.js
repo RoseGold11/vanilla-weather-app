7c72b351bdb5607948641d62fcd94533c472a411
Rachael Moten <motenrachael@gmail.com>
2022-01-19 12:59:14 -0500

    added forecast API to javascript

--- src/index.js
+++ src/index.js
@@ -15,10 +15,18 @@ function displayDate(timestamp) {
   return `${day} | ${hours} : ${minutes}`;
 }
 
-function displayForecast() {
+function findForecast(coordinates) {
+  let apiKey = `eb371e3285b37d59db8fd3917da1ca94`;
+  let forecastApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(forecastApiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
   let forecastHTML = `<div class="row">`;
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -80,6 +88,7 @@ function displayTemperature(response) {
   document.querySelector("#date").innerHTML = displayDate(
     response.data.dt * 1000
   );
+  findForecast(response.data.coord);
 }
 
 function displayFahrenheitTemperature(event) {
